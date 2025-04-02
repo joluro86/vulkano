f8af1b09fe13907650c18448c811c94bc0deed7d
joluro86 <joluro86@hotmail.com>
2025-04-02 11:20:22 -0500

    Filter empresa

--- vulkano/empresa/views.py
+++ vulkano/empresa/views.py
@@ -1,6 +1,7 @@
 # views.py
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
+from django.db.models import Q
 from .models import Empresa
 from empresa.forms.empresa_forms import EmpresaForm
 from empresa.logic.empresa_logic import create_empresa
@@ -19,4 +20,16 @@ class EmpresaCreateView(CreateView):
 class EmpresaListView(ListView):
     model = Empresa
     template_name = "empresa_list.html"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get("q")
+        if query:
+            queryset = queryset.filter(
+                Q(nombre__icontains=query) |
+                Q(nit__icontains=query) |
+                Q(direccion__icontains=query)  # Puedes agregar más filtros si es necesario
+            )
+        return queryset
+
 
