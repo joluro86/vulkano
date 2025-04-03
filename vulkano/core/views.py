356f7697f71712fef8accbc975be6aef83b91ad0
joluro86 <joluro86@hotmail.com>
2025-04-03 16:14:53 -0500

    breadcrumb

--- vulkano/core/views.py
+++ vulkano/core/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+class BreadcrumbMixin:
+    home_url = '/'
+    breadcrumb_items = []
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["home"] = self.home_url
+        context["breadcrumb_items"] = self.breadcrumb_items
+        return context
--- vulkano/empresa/views.py
+++ vulkano/empresa/views.py
@@ -5,23 +5,22 @@ from django.db.models import Q
 from empresa.models import Empresa, Sucursal
 from empresa.logic.empresa_logic import create_empresa
 from empresa.forms.empresa_forms import EmpresaEditForm, SucursalForm, EmpresaForm, SucursalEditForm
+from core.views import BreadcrumbMixin 
 
-class EmpresaCreateView(CreateView):
+class EmpresaCreateView(BreadcrumbMixin, CreateView):
     model = Empresa
     form_class = EmpresaForm
     template_name = 'crear_empresa.html'
     success_url = reverse_lazy('empresa_list')
+    breadcrumb_items = [ ("Empresas", reverse_lazy("empresa_list")),
+            ("Crear", None)]  
 
-    def form_valid(self, form):
-        # Aquí llamamos la creación de la empresa a nuestro logic
-        create_empresa(form)
-        return super().form_valid(form)
-
-class EmpresaListView(ListView):
+class EmpresaListView(BreadcrumbMixin, ListView):
     model = Empresa
     template_name = "empresa_list.html"
     ordering = ['nombre'] 
     paginate_by = 10
+    breadcrumb_items = [ ("Empresas", None), ]   
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -36,78 +35,44 @@ class EmpresaListView(ListView):
                 Q(direccion__icontains=query)  # Puedes agregar más filtros si es necesario
             )
         return queryset
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["empresa_list"] = reverse_lazy('empresa_list')
-        context["breadcrumb_items"] = [
-            ("Empresas", None),
-        ]
-        return context
 
-class EmpresaUpdateView(UpdateView):
+class EmpresaUpdateView(BreadcrumbMixin, UpdateView):
     model = Empresa
     form_class = EmpresaEditForm
     template_name = 'editar_empresa.html'
     success_url = reverse_lazy('empresa_list')
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["sucursal_list"] = reverse_lazy('sucursal_list')
-        context["breadcrumb_items"] = [
-            ("Empresas", reverse_lazy("empresa_list")),
-            ("Editar", None)
-        ]
-        return context
+    breadcrumb_items = [  ("Empresas", reverse_lazy("empresa_list")),
+            ("Editar", None) ]   
 
-
-
-class SucursalCreateView(CreateView):
+class SucursalCreateView(BreadcrumbMixin, CreateView):
     model = Sucursal
     form_class = SucursalForm
     template_name = 'crear_sucursal.html'
     success_url = reverse_lazy('sucursal_list')
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["sucursal_list"] = reverse_lazy('sucursal_list')
-        context["breadcrumb_items"] = [
-            ("Sucursales", reverse_lazy("sucursal_list")),
-            ("Crear", None)
-        ]
-        return context
-    
+    breadcrumb_items = [ ("Sucursales", reverse_lazy("sucursal_list")),
+            ("Crear", None)]    
 
-class SucursalListView(ListView):
+class SucursalListView(BreadcrumbMixin, ListView):
     model = Sucursal
     template_name = 'sucursal_list.html'
     context_object_name = 'sucursales'
     ordering = ['nombre']  # Orden alfabético por nombre de sucursal
     paginate_by = 10
+    breadcrumb_items = [("Sucursales", None)]
 
     def get_queryset(self):
         # Opcional: Prefetch para optimizar consultas de empresa asociada
         return super().get_queryset().select_related('empresa')
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["sucursal_list"] = reverse_lazy('sucursal_list')
-        context["breadcrumb_items"] = [
-            ("Sucursales", None),
-        ]
-        return context
+    
+    
+    
 
-class SucursalUpdateView(UpdateView):
+class SucursalUpdateView(BreadcrumbMixin, UpdateView):
     model = Sucursal
     form_class = SucursalEditForm
     template_name = 'editar_sucursal.html'
     success_url = reverse_lazy('sucursal_list')
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["sucursal_list"] = reverse_lazy('sucursal_list')
-        context["breadcrumb_items"] = [
-            ("Sucursales", reverse_lazy("sucursal_list")),
-            ("Editar", None)
-        ]
-        return context
+    breadcrumb_items = [("Sucursales", reverse_lazy("sucursal_list")),
+            ("Editar", None)]
+   
