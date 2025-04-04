aaa783a020a0a15db768079ac975555c55de4a7e
joluro86 <joluro86@hotmail.com>
2025-04-04 14:33:49 -0500

    breadcrumbie empresas

--- vulkano/empresa/urls.py
+++ vulkano/empresa/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from empresa.views import EmpresaCreateView, EmpresaListView, EmpresaUpdateView, SucursalUpdateView, SucursalCreateView, SucursalListView
+from empresa.views import  SucursalesPorEmpresaView, EmpresaCreateView, EmpresaListView, EmpresaUpdateView, SucursalUpdateView, SucursalCreateView, SucursalListView
 
 urlpatterns = [
     path('crear-empresa/', EmpresaCreateView.as_view(), name='crear_empresa'),
     path('empresas/', EmpresaListView.as_view(), name='empresa_list'),
     path('empresas/editar/<int:pk>', EmpresaUpdateView.as_view(), name='editar_empresa'),
     
+    path('empresas/<int:empresa_id>/sucursales/', SucursalesPorEmpresaView.as_view(), name='sucursales_por_empresa'),
     path('sucursales/', SucursalListView.as_view(), name='sucursal_list'),
     path('sucursales/crear/', SucursalCreateView.as_view(), name='crear_sucursal'),
     path('sucursales/<int:pk>/editar/', SucursalUpdateView.as_view(), name='editar_sucursal'),
-
 ]
--- vulkano/empresa/views.py
+++ vulkano/empresa/views.py
@@ -3,9 +3,10 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, UpdateView
 from django.db.models import Q
 from empresa.models import Empresa, Sucursal
-from empresa.logic.empresa_logic import create_empresa
 from empresa.forms.empresa_forms import EmpresaEditForm, SucursalForm, EmpresaForm, SucursalEditForm
-from core.views import BreadcrumbMixin 
+from core.views import BreadcrumbMixin    
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView
 
 class EmpresaCreateView(BreadcrumbMixin, CreateView):
     model = Empresa
@@ -49,8 +50,19 @@ class SucursalCreateView(BreadcrumbMixin, CreateView):
     form_class = SucursalForm
     template_name = 'crear_sucursal.html'
     success_url = reverse_lazy('sucursal_list')
-    breadcrumb_items = [ ("Sucursales", reverse_lazy("sucursal_list")),
-            ("Crear", None)]    
+
+    breadcrumb_items = [
+        ("Sucursales", reverse_lazy("sucursal_list")),
+        ("Crear", None)
+    ]
+
+    def get_initial(self):
+        initial = super().get_initial()
+        empresa_id = self.request.GET.get("empresa")
+        if empresa_id:
+            initial["empresa"] = empresa_id
+        return initial
+
 
 class SucursalListView(BreadcrumbMixin, ListView):
     model = Sucursal
@@ -58,16 +70,13 @@ class SucursalListView(BreadcrumbMixin, ListView):
     context_object_name = 'sucursales'
     ordering = ['nombre']  # Orden alfabético por nombre de sucursal
     paginate_by = 10
-    breadcrumb_items = [("Sucursales", None)]
+    breadcrumb_items = [("Empresas", reverse_lazy("empresa_list")),
+                        ("Sucursales", None)]
 
     def get_queryset(self):
         # Opcional: Prefetch para optimizar consultas de empresa asociada
         return super().get_queryset().select_related('empresa')
-    
-    
-    
-    
-
+       
 class SucursalUpdateView(BreadcrumbMixin, UpdateView):
     model = Sucursal
     form_class = SucursalEditForm
@@ -75,4 +84,16 @@ class SucursalUpdateView(BreadcrumbMixin, UpdateView):
     success_url = reverse_lazy('sucursal_list')
     breadcrumb_items = [("Sucursales", reverse_lazy("sucursal_list")),
             ("Editar", None)]
-   
+
+class SucursalesPorEmpresaView(BreadcrumbMixin, ListView):
+    model = Sucursal
+    template_name = 'lista_sucursales_por_empresa.html'
+    context_object_name = 'sucursales'
+    breadcrumb_items = [("Empresas", reverse_lazy("sucursal_list")),
+                        ("Sucursales", None)]
+
+    def get_queryset(self):
+        self.empresa = get_object_or_404(Empresa, pk=self.kwargs['empresa_id'])
+        return Sucursal.objects.filter(empresa=self.empresa)
+
+    
